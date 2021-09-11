40dda84127dcdcad82217c08503ed94c2ee4b5ca
rishika <rishika71@gmail.com>
2021-09-11 02:14:52 -0400

    - integrating google map API's

---
+++ app/src/main/java/com/example/chatroom/MapsFragment.java
@@ -0,0 +1,136 @@
+package com.example.chatroom;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.chatroom.databinding.FragmentChatroomBinding;
+import com.example.chatroom.databinding.FragmentMapsBinding;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+
+public class MapsFragment extends Fragment {
+
+    final private String TAG = "demo";
+    FragmentMapsBinding binding;
+    private GoogleMap mMap;
+    LatLng sourceLatLog;
+    LatLng destinationLatLog;
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+
+            mMap = googleMap ;
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        getActivity().setTitle("Maps");
+
+
+
+        binding = FragmentMapsBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+
+
+        String apiKey = getString(R.string.api_key);
+        // Initialize the SDK
+        Places.initialize(getContext(), apiKey);
+
+        // Create a new PlacesClient instance
+        PlacesClient placesClient = Places.createClient(getContext());
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.LAT_LNG,Place.Field.ID, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+
+                //Log.d(TAG, "onPlaceSelected: " + place.getLatLng());
+                  sourceLatLog = place.getLatLng();
+                mMap.addMarker(new MarkerOptions().position(sourceLatLog).title("Location"));
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 15);
+                mMap.animateCamera(cameraUpdate);
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+
+            }
+        });
+
+        AutocompleteSupportFragment autocompleteFragment2 = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment2);
+
+        autocompleteFragment2.setPlaceFields(Arrays.asList(Place.Field.LAT_LNG,Place.Field.ID, Place.Field.NAME));
+
+        autocompleteFragment2.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+
+                //Log.d(TAG, "onPlaceSelected: " + place.getLatLng());
+                destinationLatLog = place.getLatLng();
+                mMap.addMarker(new MarkerOptions().position(destinationLatLog).title("Location"));
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 15);
+                mMap.animateCamera(cameraUpdate);
+            }
+
+
+            @Override
+            public void onError(@NonNull Status status) {
+
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+}
