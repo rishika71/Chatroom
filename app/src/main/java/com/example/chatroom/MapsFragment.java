915ab35ee38fdd65497a7976f2c731990ddcdfd9
rishika <rishika71@gmail.com>
2021-09-12 19:09:53 -0400

    - UI changes in Map Fragment

--- app/src/main/java/com/example/chatroom/MapsFragment.java
+++ app/src/main/java/com/example/chatroom/MapsFragment.java
@@ -1,16 +1,32 @@
 package com.example.chatroom;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.chatroom.databinding.FragmentMapsBinding;
+import com.example.chatroom.models.Chat;
+import com.example.chatroom.models.User;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,11 +34,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.Arrays;
 
@@ -34,14 +55,46 @@ public class MapsFragment extends Fragment {
     LatLng sourceLatLog;
     LatLng destinationLatLog;
 
+    Location currentLocation;
+    FusedLocationProviderClient mFusedLocationClient;
+    public static final int REQUEST_LOCATION = 72;
+
+    IMap am;
+    User user;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof IMap) {
+            am = (IMap) context;
+        } else {
+            throw new RuntimeException(context.toString());
+        }
+        user = am.getUser();
+    }
+
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         @Override
         public void onMapReady(GoogleMap googleMap) {
 
+            LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You");
+            googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+            googleMap.addMarker(markerOptions);
             mMap = googleMap ;
+
         }
     };
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        getLastLocation();
+    }
 
     @Nullable
     @Override
@@ -51,17 +104,20 @@ public class MapsFragment extends Fragment {
 
         getActivity().setTitle("Maps");
 
-
-
         binding = FragmentMapsBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+        if (user.getPhotoref() != null) {
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference().child(user.getId()).child(user.getPhotoref());
+            GlideApp.with(view)
+                    .load(storageReference)
+                    .into(binding.riderImage);
+        } else {
+            GlideApp.with(view)
+                    .load(R.drawable.profile_image)
+                    .into(binding.riderImage);
         }
-
+        binding.riderName.setText(user.getFirstname() + " " + user.getLastname());
 
         String apiKey = getString(R.string.api_key);
         // Initialize the SDK
@@ -128,4 +184,60 @@ public class MapsFragment extends Fragment {
 
     }
 
+    @SuppressLint("MissingPermission")
+    public void getLastLocation() {
+        if (hasLocationPerms()) {
+            LocationManager lm = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+            if (isLocationEnabled(lm)) {
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        Location location = task.getResult();
+                        if (location != null) {
+                            currentLocation = location;
+                            SupportMapFragment mapFragment =
+                                    (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+                            if (mapFragment != null) {
+                                mapFragment.getMapAsync(callback);}
+
+                        }
+                    }
+                });
+            } else {
+                Toast.makeText(getContext(), "Please turn your location on!", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            requestLocationPerms();
+        }
+    }
+
+    public boolean isLocationEnabled(LocationManager lm) {
+        return lm.isProviderEnabled(LocationManager.GPS_PROVIDER) || lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    public void requestLocationPerms() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+    }
+
+    public boolean hasLocationPerms() {
+        return ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    interface IMap {
+
+        User getUser();
+
+    }
 }
